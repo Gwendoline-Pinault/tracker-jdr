b5c971eacbb1c517e2b01ddc583a804b45a9ed08
Gwendoline-Pinault <pinaultgwendoline@gmail.com>
2026-01-20 17:20:03 +0100

    change relative path by path.join

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ require('dotenv').config()
 const express = require('express');
 const router = require('./app/router');
 const session = require('express-session');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.set('view engine', 'ejs');
-app.set('views', './app/views');
+app.set('views', path.join(__dirname, '/app/views') );
 
-app.use('/static', express.static('./public'));
+app.use('/static', express.static(path.join(__dirname,'/public')));
 app.use(express.urlencoded({extended: true}));
 
 app.use(session({
